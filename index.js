3f1cc14ca644455be123cbf65d32c6376a2b1fc1
BitTim <tim.anhalt.2004@protonmail.com>
2020-03-22 13:43:32 +0100

    Added "Say" function

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const bot = new Discord.Client()
 console.log("Getting Token")
 const token = fs.readFileSync("token.txt", "utf8").split("\n")[0]
 
+const PREFIX = '='
+const replies = ["No.", "I don't want to", "Do I really have to say that?", "Nope", "I hate you"]
+
 bot.on("ready", () => {
 	console.log("Bot started")
+})
+
+bot.on("message", msg=> {
+	console.log("Received: " + msg.content)
+	let data = msg.content.substr(PREFIX.length).split(" ")
+
+	if(data[0] === "say") 
+	{
+		let i = Math.floor(Math.random() * Math.floor(replies.length))
+		msg.channel.send(replies[i])
+	}
 })
 
 console.log("Logging in")
