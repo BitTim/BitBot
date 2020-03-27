ac244573e584ef14bfc431903d056a34b2f68a5f
BitTim <tim.anhalt.2004@protonmail.com>
2020-03-27 16:36:45 +0100

    Started on bit system

---
+++ commands/bits.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+const fs = require("fs")
+
+module.exports = {
+  name: "bits",
+  description: "Operations with bits",
+  exec(msg, args)
+  {
+    
+  }
+}
---
+++ commands/gamble.js
@@ -0,0 +1,10 @@
+const Discord
+
+module.exports = {
+  name: "gamble",
+  description: "Lets you gamble with bits",
+  exec(msg, args)
+  {
+
+  }
+}
