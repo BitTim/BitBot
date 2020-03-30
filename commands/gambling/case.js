efad8edd2fe44ffb4b9650d830a6999f42f75af4
BitTim <tim.anhalt.2004@protonmail.com>
2020-03-30 14:55:26 +0200

    Fixed bug with underflow protection

--- commands/gambling/case.js
+++ commands/gambling/case.js
@@ -86,7 +86,7 @@ module.exports = {
       loan = true;
     }
 
-    if(db.find(user => user.id === msg.author.id).bits < 1000000000)
+    if(db.find(user => user.id === msg.author.id).bits < -1000000000)
     {
       embed.setTitle("🔴 You have used to much of your loan, get funds from other users");
       msg.channel.send(embed);
